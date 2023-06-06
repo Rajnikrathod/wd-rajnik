3c02a55644a417321a1f5fbdebe70ef5e0d867db
Rajnikrathod <115615523+Rajnikrathod@users.noreply.github.com>
2023-06-06 14:05:05 +0530

    Add files via upload

---
+++ wd -rajnik/project/project.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#web').click(function(){
+        $('.text').html('<h1>Featured Content Title</h1>'+'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur doloremque deserunt, similique amet numquam excepturi veritatis modi fuga assumenda delectus placeat enim adipisci architecto et nulla magnam illo quibusdam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur doloremque deserunt, similique amet numquam excepturi veritatis modi fuga assumenda delectus placeat enim adipisci architecto et nulla magnam illo quibusdam</p>')
+    })
+})
+$(document).ready(function(){
+    $('#logo').click(function(){
+        $('.text').html('<h1>logo</h1>'+'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur doloremque deserunt, similique amet numquam excepturi veritatis modi fuga assumenda delectus placeat enim adipisci architecto et nulla magnam illo quibusdam </p>')
+    })
+})
+$(document).ready(function(){
+    $('#banner').click(function(){
+        $('.text').html('<h1>banner</h1>'+'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur doloremque deserunt, similique amet numquam excepturi veritatis modi fuga assumenda delectus placeat enim adipisci architecto et nulla magnam illo quibusdam</p>')
+    })
+})
+$(document).ready(function(){
+    $('#flayer').click(function(){
+        $('.text').html('<h1>Flayer</h1>'+'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur doloremque deserunt, similique amet numquam excepturi veritatis modi fuga assumenda delectus placeat enim adipisci architecto et nulla magnam illo quibusdam</p>')
+    })
+})
